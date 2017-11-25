228e48dd3804680e0d0c7ab696512608a71e61f0
KarthikeyanYuvaraj <KarthikeyanYuvaraj@yahoo.com>
2017-11-25 12:06:41 +0530

    CarInventory.Service

---
+++ CarInventory.Services/Services/CarService.cs
@@ -0,0 +1,19 @@
+﻿using CarInventory.Core.Data;
+using CarInventory.Core.Entities;
+using CarInventory.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventory.Services.Services
+{
+    public class CarService : BaseService<Car>, ICarService
+    {
+        public CarService(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+}
