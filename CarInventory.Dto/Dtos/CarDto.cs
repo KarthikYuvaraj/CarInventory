66e29394d6640c399b0bde8d7eab91578bfe72c2
KarthikeyanYuvaraj <KarthikeyanYuvaraj@yahoo.com>
2017-11-25 11:18:49 +0530

    Created Dtos

---
+++ CarInventory.Dto/Dtos/CarDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventory.Dto.Dtos
+{
+    class CarDto
+    {
+        public CarDto()
+        {
+
+        }
+
+        public int CarId { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public Boolean NewCar { get; set; }
+    }
+}
